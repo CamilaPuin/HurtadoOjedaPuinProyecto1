260910d36b17ec36b1823735505ba5fe52d370ef
CamilaPuin <diana.puin01@uptc.edu.co>
2025-04-03 18:52:16 -0500

    New prueba de rama

--- src/co/edu/uptc/presenter/App.java
+++ src/co/edu/uptc/presenter/App.java
@@ -5,7 +5,10 @@ import co.edu.uptc.view.View;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        
         View view = new View();
         //esta es una prueba de mi nueva rama :) (Camila)
+        //prueba de pull request directo 
+
     }
 }

6cf621e6975ff4eb3762fc644e6046dc0b958290
0jed1ta <juan.ojeda@uptc.edu.co>
2025-04-03 19:30:25 -0500

    Primer commit Juan Manuel Rama

--- src/co/edu/uptc/presenter/App.java
+++ src/co/edu/uptc/presenter/App.java
@@ -2,6 +2,7 @@ package co.edu.uptc.presenter;
 
 import co.edu.uptc.presenter.Presenter;
 import co.edu.uptc.view.View;
+//HOLA MANI
 
 public class App {
     public static void main(String[] args) throws Exception {

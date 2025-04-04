a2373b109e58d378606ceeb7016d5ebbd7fedd9b
SantiagoHurtadoNieto <santiago.hurtado02@uptc.edu.co>
2025-04-03 19:37:12 -0500

    Firts Commit

--- src/co/edu/uptc/view/View.java
+++ src/co/edu/uptc/view/View.java
@@ -17,7 +17,6 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
